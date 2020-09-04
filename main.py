d2b1374caed7cf33efe1e94207d4d5a42afc56b5
HStep20 <Stephens.j.hunter@protonmail.com>
2020-09-04 00:25:15 -0600

    -ADD: Scoped Variables to change Neural Network Parameters; Files are named based on parameters for ease of comparison; Parameters and time to run added as a header to the output file -REMOVE: Learn large texts -TODO: Cleanup Code; Add more generation specification options; Create new generative classes for things besides names

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
+from textgenrnn import textgenrnn
+import os
+import time
+
+start_time = time.time()
+
+rnnLayers = 8 #Number of Recursive Neural Network Layers | Higher == random letters
+#Size of RNN Layers | Higher == Random Letters
+rnnSize = 256
+batchSize = 256 #
+trainSize = .7 #Uses x% of data to learn from. 100% means exact same output as input
+numEpochs = 100
+genEpochs = 10 #spits out output every x epochs
+maxInputLength = 50
+maxGenLength = 25
+bidirectional = True
+
+#Output Variables
+outputMaxGenLength = 200
+outputInitialCount = 250
+outputTemp = .75
+
 def learnnames():
     textgen.reset()
     textgen.train_from_file(
         textpath,
         name=modelname,
         word_level=False,
-        max_length=50,
-        batch_size=1024,
-        max_gen_length=25,
+        max_length=maxInputLength,
+        batch_size=batchSize,
+        max_gen_length=maxGenLength,
         new_model=True,
-        rnn_layers=8, #higher == random letters
-        rnn_bidirectional=False,
-        rnn_size=128, #too high == random letters
+        rnn_layers=rnnLayers,
+        rnn_bidirectional=bidirectional,
+        rnn_size=rnnSize,
         dim_embeddings=250,
-        train_size=0.75,
-        num_epochs=500,
-        gen_epochs=500
-    )
-    textgen.save(savepath)
-def learnbook():
-    textgen.reset()
-    textgen.train_from_file(
-        textpath,
-        name=modelname,
-        word_level=False,#True for the novels
-        max_length=50, #(25) Lower for World Leve
-        #max_gen_length=50, #Lower for Word Level, but higher for long texts
-        #batch_size=256,
-        #max_words=1000,
-        new_model=True,
-        rnn_layers=4,
-        #rnn_bidirectional=True,
-        rnn_size=128,
-        #dim_embeddings=250,
-        num_epochs=10,
-        gen_epochs=25,
-        train_size=0.75
+        train_size=trainSize,
+        num_epochs=numEpochs,
+        gen_epochs=genEpochs
     )
     textgen.save(savepath)
 def printoutput():
@@ -43,14 +45,13 @@ def printoutput():
         vocab_path=modelname+'_vocab.json',
         config_path=modelname+'_config.json'
     )
-    if "names" in text:
-        textgen.generate_to_file(outputpath, max_gen_length=200, n=250, temperature=.5)
-        cleantext()
-    else:
-        textgen.generate_to_file(outputpath, max_gen_length=250, n=5, temperature=.2)
+    textgen.generate_to_file(outputpath, max_gen_length=outputMaxGenLength, n=outputInitialCount, temperature=outputTemp)
+    #TODO - Add something to specify starting letters of names with the textgenrnn lib
+    cleantext()
+
 
 def cleantext():
-    cleanpath = './outputs/' + text + '.txt'
+    cleanpath = './outputs/' + text + ", layers-"+str(rnnLayers) + ", size-" + str(rnnSize) + ", epochs-"+str(numEpochs)+ ", bidirectional-"+str(bidirectional) + ", outputTemperature-" + str(outputTemp) + '.txt'
     masterfile = open(textpath, "r", encoding="UTF-8")
     uncleanfile = open(outputpath, "r", encoding="UTF-8")
     cleanfile = open(cleanpath, "w+", encoding="UTF-8")
@@ -64,6 +65,26 @@ def cleantext():
     for line in same:
         words.append(line)
     words.sort()
+    cleanfile.write(
+        "Time to execute: " + ("%s2 seconds" % round((time.time() - start_time), 2)) +
+        "\nTime to execute: " + ("%s2 minutes" % round(((time.time() - start_time)/60), 2)) +
+        "\n\n========== Model Info ==========" +
+        "\nrnnLayers: " + str(rnnLayers) +
+        "\nrnnSize: " + str(rnnSize) +
+        "\nbatchSize: " + str(batchSize) +
+        "\ntrainSize: " + str(trainSize) +
+        "\nbidirectional: " + str(bidirectional) +
+        "\nnumEpochs: " + str(numEpochs) +
+        #"\nmaxInputLength: " + maxInputLength +
+        "\nmaxGenLength: " + str(maxGenLength) +
+        "\n\n========== Output Info =========="+
+        "\nOutput Temp: " + str(outputTemp) +
+        #"\nOutput Count: " + str(outputInitialCount) +
+        "\nOutput Total: " + str(len(words)) +
+
+        "\n===================================================\n\n"
+    )
+
 
     for i in words:
         cleanfile.write(i)
@@ -74,12 +95,8 @@ def cleantext():
     cleanfile.close()
     os.remove(outputpath)
 
-
-from textgenrnn import textgenrnn
-import os
-
 textgen = textgenrnn()
-text = 'lotr'
+text = 'slanames'
 learn = True
 
 newpath= r'./models/'+text
@@ -89,16 +106,10 @@ if not os.path.exists(newpath):
 modelname = './models/' + text +'/'+ text + '_model'
 textpath = './texts/' + text + '.txt'
 savepath = './models/' + text +'/'+ text+ '.hdf5'
-
-if "names" in text:
-    outputpath = './outputs/' + text + 'Unclean.txt'
-else:
-    outputpath = './outputs/' + text + '.txt'
+outputpath = './outputs/' + text + 'Unclean.txt'
 
 
 if learn == True:
-    if "names" in text:
-        learnnames()
-    else:
-        learnbook()
+    learnnames()
+
 printoutput()
